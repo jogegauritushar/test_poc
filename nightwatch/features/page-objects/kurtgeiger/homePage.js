6c36ec2558370d5cfd4605d38baae4ec8a7c4923
Gauri Joge <gauri.joge@MACPRO-02.local>
2017-12-01 12:17:35 +0000

    homepage modified

--- nightwatch/features/page-objects/kurtgeiger/homePage.js
+++ nightwatch/features/page-objects/kurtgeiger/homePage.js
@@ -1,14 +1,29 @@
 module.exports = {
 	elements: {
 		body: 'body',
-		headerCategory: {selector: "//div[@class='inner-wrap']/ul/li[2]/a", locateStrategy: 'xpath'}
+		headerCategory: {selector: "//div[@class='inner-wrap']/ul/li[2]/a", locateStrategy: 'xpath'},
+		signInTab: {selector: '//div[@id="ss17-wrapLinksSearch"]/ul/li[7]/a', locateStrategy: 'xpath'},
+		//menuList: {selector: "//div[@class='inner-wrap']/ul/li/a", locateStrategy: 'xpath'}
 	},
 	commands: [
 	{
 		chooseHeaderCategory: function(){
-			 return this
-			.waitForElementVisible('@headerCategory', 2000)
-			.click('@headerCategory');
+			 this
+			.waitForElementVisible('@headerCategory', 1000)
+			.click('@headerCategory')
+			return this;
 		},
+
+		signIn: function(){
+			this
+			.waitForElementVisible('@signInTab', 2000)
+			.click('@signInTab')
+			return this;
+	}
+         /*menuListValidation: function() {
+         	return this.api.elements('xpath', "//div[@class='inner-wrap']/ul/li/a").length
+       
+ 			console.log(result.length);
+ }*/
 	}]
 };
