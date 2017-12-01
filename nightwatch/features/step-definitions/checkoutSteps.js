fe127d988f1be023f2c0eaaec26cdb07a08f8bfd
Gauri Joge <gauri.joge@MACPRO-02.local>
2017-12-01 12:14:09 +0000

    modified checkout

--- nightwatch/features/step-definitions/checkoutSteps.js
+++ nightwatch/features/step-definitions/checkoutSteps.js
@@ -3,56 +3,55 @@ const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(({Given, Then, When, Before, After}) => {
 
-    let hp = client.page.shoeaholics.homePage();
-    let plp = client.page.shoeaholics.plpPage();
-    let pdp = client.page.shoeaholics.pdpPage();
-    let cp = client.page.shoeaholics.cartPage();
-    let hp = client.page.kurtgeiger.homePage();
-    let plp = client.page.kurtgeiger.plpPage();
-    let pdp = client.page.kurtgeiger.pdpPage();
-    let cp = client.page.kurtgeiger.cartPage();
+    let launchUrl = client.launch_url;
+    let hp = null;
+    let plp = null;
+    let pdp = null;
+    let cp = null;
 
+    if (launchUrl.search('shoeaholics') !== -1) {
+        hp = client.page.shoeaholics.homePage();
+        plp = client.page.shoeaholics.plpPage();
+        pdp = client.page.shoeaholics.pdpPage();
+        cp = client.page.shoeaholics.cartPage();
+    }
 
-    Given('I am on the homepage', function (homepage){
+    if (launchUrl.search('kurtgeiger') !== -1) {
+        hp = client.page.kurtgeiger.homePage();
+        plp = client.page.kurtgeiger.plpPage();
+        pdp = client.page.kurtgeiger.pdpPage();
+        cp = client.page.kurtgeiger.cartPage();
+    }
+
+    Given(/^I am on the homepage$/, () => {
         client.maximizeWindow();
-        return client
-            .waitForElementVisible('body', 1000)
-            .assert.containsText('body', homepage);
+         client
+            .waitForElementVisible('body', 1000);
+
     });
 
     Given(/^I am on PDP$/, function () {
-        console.log('dhfgrgfgfd-----');
-         client.pause(1000);
-         hp.chooseHeaderCategory()
-         return plp.selectProduct();
+          hp.chooseHeaderCategory()
+            return plp.selectProduct()
     });
 
     When(/^I select any colour and size$/, function () {
-        console.log('dhfgrgfgfd-----');
         return pdp.selectColourAndSize();
     });
 
     When(/^I add the product to the bag$/, function () {
-        console.log('dhfgrgfgfd-----');
         return pdp.addToBag();
     });
 
     Then(/^I should go to 'My Bag' and assert bag quantity$/, function () {
-        return client
-        console.log('dhfgrgfgfd-----');
         return  pdp.checkMyBag();
     });
 
-    Then(/^I click 'Proceed to checkout'$/, function () {
-        console.log('dhfgrgfgfd-----');
+    Then(/^I click 'Proceed to Checkout'$/, function () {
         return cp.proceedToCheckout();
     });
     When(/^I should be on Registeration page$/, function () {
 
-        return client 
-        .useCss()
-        .waitForElementVisible("body", 2000)
-        //.expect.element("body[class=' uk checkout-onepage-index']").text.to.contain('Sign in / Create an account');
-        .assert.containsText("body",'Shoeaholics');
+        client.assert.urlContains('checkout')
     });
 })
